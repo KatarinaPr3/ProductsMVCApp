33337946dbfbfe9c250886582220fe02e7e89676
KatarinaPr3 <prljickatarina@gmail.com>
2022-09-23 21:49:47 +0200

    A

--- ProductsMVCApp/Controllers/ProductController.cs
+++ ProductsMVCApp/Controllers/ProductController.cs
@@ -5,52 +5,129 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data;
 using System.Data.SqlClient;
+using ProductsMVCApp.Models;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace ProductsMVCApp.Controllers
 {
     public class ProductController : Controller
     {
-        string connString = @"Data Source =. ;Initial Catalog = ProductsDB; Integrated Security = true";
+        private string connString = @"Data Source =. ;Initial Catalog = ProductsDB; Integrated Security = true";
+        private string path = AppDomain.CurrentDomain.BaseDirectory;
+        private int jsonId;
+        private string fileName = "jsonProducts.json";
+        List<ProductModel> products = new List<ProductModel>();
 
         [HttpGet]
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             DataTable dt = new DataTable();
-            using (SqlConnection con = new SqlConnection(connString))
+            try
             {
-                con.Open();
-                string query = "Select * FROM Products";
-                SqlDataAdapter sda = new SqlDataAdapter(query, con);
-                sda.Fill(dt);
-
+                using (SqlConnection con = new SqlConnection(connString))
+                {
+                    con.Open();
+                    string query = "Select * FROM Products";
+                    SqlDataAdapter sda = new SqlDataAdapter(query, con);
+                    sda.Fill(dt);
+                }
+                if (System.IO.File.Exists(Path.Combine(path, fileName)))
+                {
+                    string filePath = Path.Combine(path, fileName);
+                    string jsonTxt = System.IO.File.ReadAllText(filePath);
+                    products = JsonConvert.DeserializeObject<List<ProductModel>>(jsonTxt);
+                }
+                else
+                {
+                    string filePath = Path.Combine(path, fileName);
+                    System.IO.File.Create(filePath);
+                }
             }
-            return View(dt);
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in: " + ex.ToString());
+            }           
+            return await Task.Run(() => View(dt));
         }
 
-        // GET: Product/Details/5
-        public ActionResult Details(int id)
+        public ActionResult IndexJSON()
         {
-            return View();
+            List<ProductModel> products = new List<ProductModel>();
+            try
+            {
+                if (System.IO.File.Exists(Path.Combine(path, fileName)))
+                {
+                    string filepath = Path.Combine(path, fileName);
+                    string jsonTxt = System.IO.File.ReadAllText(filepath);
+                    products = JsonConvert.DeserializeObject<List<ProductModel>>(jsonTxt);
+                }
+                if (products != null)
+                {
+                    return View(products);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in: " + ex.ToString());
+                return View();
+            }
         }
 
+        [HttpGet] 
         // GET: Product/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new ProductModel());
         }
 
         // POST: Product/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(ProductModel productModel)
         {
             try
             {
-                // TODO: Add insert logic here
-
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    string query = "Insert into Products Values (@ProductName, @ProductDescription, @ProductCategory, @ProductManufacturer, @ProductSupplier, @ProductPrice)";
+                    string queryId = "Select MAX(ProductID) FROM Products";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@ProductName", productModel.ProductName);
+                    cmd.Parameters.AddWithValue("@ProductDescription", productModel.ProductDescription);
+                    cmd.Parameters.AddWithValue("@ProductCategory", productModel.ProductCategory);
+                    cmd.Parameters.AddWithValue("@ProductManufacturer", productModel.ProductManufacturer);
+                    cmd.Parameters.AddWithValue("@ProductSupplier", productModel.ProductSupplier);
+                    cmd.Parameters.AddWithValue("@ProductPrice", productModel.ProductPrice);
+                    cmd.ExecuteNonQuery();
+                    SqlCommand cmdID = new SqlCommand(queryId, conn);
+                    jsonId = (Int32)cmdID.ExecuteScalar();
+                }
+                string filePath = Path.Combine(path, fileName);
+                if (jsonId == 0)
+                {
+                    jsonId = products.Count + 1;
+                }
+                productModel.ProductID = jsonId;
+                string jsonTxt = System.IO.File.ReadAllText(filePath); 
+                products = JsonConvert.DeserializeObject<List<ProductModel>>(jsonTxt);
+                if (products == null)
+                {
+                    products = new List<ProductModel>();
+                }
+                products.Add(productModel);
+                string json = JsonConvert.SerializeObject(products, Formatting.Indented);
+                System.IO.File.WriteAllText(filePath, json);
                 return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
+                Console.WriteLine("Exception in: " + ex.ToString());
                 return View();
             }
         }
@@ -58,21 +135,77 @@ namespace ProductsMVCApp.Controllers
         // GET: Product/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            ProductModel productModel = new ProductModel();
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    string query = "Select * from Products where ProductID = @ProductID";
+                    SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+                    sda.SelectCommand.Parameters.AddWithValue("@ProductID", id);
+                    sda.Fill(dt);
+                }
+                if (dt.Rows.Count == 1)
+                {
+                    productModel.ProductID = Convert.ToInt32(dt.Rows[0][0].ToString());
+                    productModel.ProductName = dt.Rows[0][1].ToString();
+                    productModel.ProductDescription = dt.Rows[0][2].ToString();
+                    productModel.ProductCategory = dt.Rows[0][3].ToString();
+                    productModel.ProductManufacturer = dt.Rows[0][4].ToString();
+                    productModel.ProductSupplier = dt.Rows[0][5].ToString();
+                    productModel.ProductPrice = Convert.ToDecimal(dt.Rows[0][6].ToString());
+                    return View(productModel);
+                }
+                else return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in: " + ex.ToString());
+                return View();
+            }
         }
 
         // POST: Product/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(ProductModel productModel)
         {
             try
             {
-                // TODO: Add update logic here
-
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    string query = "Update Products Set ProductName = @ProductName, ProductDescription = @ProductDescription, ProductCategory = @ProductCategory, ProductManufacturer = @ProductManufacturer, ProductSupplier = @ProductSupplier, ProductPrice = @ProductPrice where ProductID = @ProductID";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@ProductID", productModel.ProductID);
+                    cmd.Parameters.AddWithValue("@ProductName", productModel.ProductName);
+                    cmd.Parameters.AddWithValue("@ProductDescription", productModel.ProductDescription);
+                    cmd.Parameters.AddWithValue("@ProductCategory", productModel.ProductCategory);
+                    cmd.Parameters.AddWithValue("@ProductManufacturer", productModel.ProductManufacturer);
+                    cmd.Parameters.AddWithValue("@ProductSupplier", productModel.ProductSupplier);
+                    cmd.Parameters.AddWithValue("@ProductPrice", productModel.ProductPrice);
+                    cmd.ExecuteNonQuery();
+                }
+                string filePath = Path.Combine(path, fileName);
+                string jsonTxt = System.IO.File.ReadAllText(filePath);
+                products = JsonConvert.DeserializeObject<List<ProductModel>>(jsonTxt);
+                var productsFromJson = jsonTxt;
+                for (int i = 0; i < products.Count; i++)
+                {
+                    if (products[i].ProductID == productModel.ProductID)
+                    {
+                        products[i] = productModel;
+                        var jsonForSerialize = JsonConvert.SerializeObject(products, Formatting.Indented);
+                        System.IO.File.WriteAllText(filePath, jsonForSerialize);
+                        break;
+                    }
+                }             
                 return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
+                Console.WriteLine("Exception in: " + ex.ToString());
                 return View();
             }
         }
@@ -80,7 +213,36 @@ namespace ProductsMVCApp.Controllers
         // GET: Product/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            ProductModel productModel = new ProductModel();
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    string query = "Select * from Products where ProductID = @ProductID";
+                    SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+                    sda.SelectCommand.Parameters.AddWithValue("@ProductID", id);
+                    sda.Fill(dt);
+                }
+                if (dt.Rows.Count == 1)
+                {
+                    productModel.ProductID = Convert.ToInt32(dt.Rows[0][0].ToString());
+                    productModel.ProductName = dt.Rows[0][1].ToString();
+                    productModel.ProductDescription = dt.Rows[0][2].ToString();
+                    productModel.ProductCategory = dt.Rows[0][3].ToString();
+                    productModel.ProductManufacturer = dt.Rows[0][4].ToString();
+                    productModel.ProductSupplier = dt.Rows[0][5].ToString();
+                    productModel.ProductPrice = Convert.ToDecimal(dt.Rows[0][6].ToString());
+                    return View(productModel);
+                }
+                else return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in: " + ex.ToString());
+                return View();
+            }
         }
 
         // POST: Product/Delete/5
@@ -89,14 +251,34 @@ namespace ProductsMVCApp.Controllers
         {
             try
             {
-                // TODO: Add delete logic here
-
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    string query = "Delete from Products where ProductID = @ProductID";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@ProductID", id);
+                    cmd.ExecuteNonQuery();
+                }
+                string filePath = Path.Combine(path, fileName);
+                string jsonTxt = System.IO.File.ReadAllText(filePath);
+                products = JsonConvert.DeserializeObject<List<ProductModel>>(jsonTxt);
+                for (int i = 0; i < products.Count; i++)
+                {
+                    if (products[i].ProductID == id)
+                    {
+                        products.Remove(products[i]);
+                        var jsonForSerialize = JsonConvert.SerializeObject(products, Formatting.Indented);
+                        System.IO.File.WriteAllText(filePath, jsonForSerialize);
+                        break;
+                    }
+                }
                 return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
+                Console.WriteLine("Exception in: " + ex.ToString());
                 return View();
-            }
+            }            
         }
     }
 }
---
+++ ProductsMVCApp/Models/ProductModel.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ProductsMVCApp.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [DisplayName("Product Name")]
+        [DataType(DataType.Text, ErrorMessage = "Insert valid name!")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string ProductName { get; set; }
+
+        [DisplayName("Product Description")]
+        [DataType(DataType.Text, ErrorMessage = "Insert valid description!")]
+        [StringLength(100, MinimumLength = 2)]
+        [Required]
+        public string ProductDescription { get; set; }
+
+        [DisplayName("Product Category")]
+        [DataType(DataType.Text, ErrorMessage = "Insert valid Category for Product!")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string ProductCategory { get; set; }
+
+        [DisplayName("Product Manufacturer")]
+        [DataType(DataType.Text, ErrorMessage = "Insert valid manufacturer!")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string ProductManufacturer { get; set; }
+
+        [DisplayName("Product Supplier")]
+        [DataType(DataType.Text, ErrorMessage = "Insert valid supplier!")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required]
+        public string ProductSupplier { get; set; }
+
+        [DisplayName("Product Price")]
+        [DataType(DataType.Currency, ErrorMessage = "Insert valid value!")]
+        [Range(0.01, 1000000, ErrorMessage = "Price of product can not be smaller than 0 or bigger than 1000000.")]
+        [Required]
+        public decimal ProductPrice { get; set; }
+    }
+}
